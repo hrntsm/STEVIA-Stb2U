31741da7fbbd83e3a1b1040faf43e9f3c55817a2
hiron_rgkr <hiroaki.natsume@outlook.com>
2020-02-05 20:27:27 +0900

    [add] colorpicker can change checkpanel  color
    テストでカラーピッカーからパネルの色を変える部分を追加

--- Assets/Scripts/ColorPicker.cs
+++ Assets/Scripts/ColorPicker.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // 参考 http://backlight1162.blog.fc2.com/blog-entry-85.html
 public class ColorPicker : MonoBehaviour
@@ -13,12 +14,15 @@ public class ColorPicker : MonoBehaviour
     public int y = 100;
 
     void OnGUI() {
+        GameObject obj;
         if (GUI.RepeatButton(new Rect(x, y, ImageWidth, ImageHeight), colorPicker)) {
             Vector2 pickpos = Event.current.mousePosition;
             int aaa = Convert.ToInt32(pickpos.x);
             int bbb = Convert.ToInt32(pickpos.y);
             Color col = colorPicker.GetPixel(aaa, 41 - bbb);
 
+            obj = GameObject.Find("ColorCheckPanel");
+            obj.GetComponent<Image>().color = col;
             Debug.Log(col);
         }
     }
