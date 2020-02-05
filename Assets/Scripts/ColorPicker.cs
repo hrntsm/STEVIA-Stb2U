8c66ef331f6ac34351072afa71a9c0cc615e80e2
hiron_rgkr <hiroaki.natsume@outlook.com>
2020-02-05 20:01:04 +0900

    [add] test colorpicker
    カラーピッカーをテストで設置

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 参考 http://backlight1162.blog.fc2.com/blog-entry-85.html
+public class ColorPicker : MonoBehaviour
+{
+    public Texture2D colorPicker;
+    public int ImageWidth = 100;
+    public int ImageHeight = 100;
+    public int x = 100;
+    public int y = 100;
+
+    void OnGUI() {
+        if (GUI.RepeatButton(new Rect(x, y, ImageWidth, ImageHeight), colorPicker)) {
+            Vector2 pickpos = Event.current.mousePosition;
+            int aaa = Convert.ToInt32(pickpos.x);
+            int bbb = Convert.ToInt32(pickpos.y);
+            Color col = colorPicker.GetPixel(aaa, 41 - bbb);
+
+            Debug.Log(col);
+        }
+    }
+}
