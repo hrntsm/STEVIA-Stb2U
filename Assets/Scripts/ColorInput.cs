d42cae6bea827d331cbd4456d819a467f3d2d3ab
hiron_rgkr <hiroaki.natsume@outlook.com>
2020-02-12 18:26:17 +0900

    [add] saved color information to PlayerPrefab and load it
    入力した色情報をPlayerPrefabに保存し読み込めるようにした

--- Assets/Scripts/ColorInput.cs
+++ Assets/Scripts/ColorInput.cs
@@ -6,35 +6,55 @@ using UnityEngine.UI;
 
 public class ColorInput:MonoBehaviour {
     InputField inputField;
-    string inputText;
     public static Color[] memberColor = new Color[11];
+    public static SaveColor sCol = new SaveColor();
     // Start is called before the first frame update
     void Start() {
         inputField = GetComponent<InputField>();
-        InitMemberColor();
-
         // Get the color setting of the past
-        //if (PlayerPrefs.HasKey("MemberColor")) {
-        //    memberColor = PlayerPrefs.GetString("MemberColor");
-        //}
-        //else {
-        //    InitMemberColor();
-        //}
+        if (PlayerPrefs.HasKey("UserColorData"))
+            InitMemberColor(true);
+            //InitMemberColor(false);
+        else
+            InitMemberColor(false);
     }
 
     public void ChangeColor(int i) {
-        inputText = inputField.text;
-        float[] rgba = Array.ConvertAll<string, float>(inputText.Split(','), float.Parse);
-        memberColor[i] = new Color(rgba[0], rgba[1], rgba[2], rgba[3]);
+        memberColor[i] = GetMemberColor(inputField.text);
         inputField.GetComponent<Image>().color = memberColor[i];
+
+        sCol.num[i] = i;
+        sCol.rgba[i] = inputField.text;
     }
 
-    void InitMemberColor() {
+    void InitMemberColor(bool init) {
         int i = 0;
-        while (i < 11) {
-            memberColor[i] = new Color(1, 1, 1, 1);
-            i++;
+        if (init) {
+            string json = PlayerPrefs.GetString(SaveColorData._SaveKey);
+            print(json);
+            SaveColor saveColor = JsonUtility.FromJson<SaveColor>(json);
+            while (i < 11) {
+                string inputText = saveColor.rgba[i];
+                memberColor[i] = GetMemberColor(inputText);
+                sCol.num[i] = saveColor.num[i];
+                sCol.rgba[i] = saveColor.rgba[i];
+                i++;
+            }
         }
+        else {
+            while (i < 11) {
+                memberColor[i] = new Color(1, 1, 1, 1);
+                sCol.num[i] = i;
+                sCol.rgba[i] = "1,1,1,1";
+                i++;
+            }
+        }
+    }
+
+    Color GetMemberColor(string inputText) {
+        float[] inputRGBA = Array.ConvertAll<string, float>(inputText.Split(','), float.Parse);
+        Color color = new Color(inputRGBA[0], inputRGBA[1], inputRGBA[2], inputRGBA[3]);
+        return (color);
     }
 
 }
---
+++ Assets/Scripts/SaveColorData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveColorData: MonoBehaviour{
+    public const string _SaveKey = "UserColorData";
+
+    public void Save() {
+        string json = JsonUtility.ToJson(ColorInput.sCol);
+        PlayerPrefs.SetString(_SaveKey, json);
+        Debug.Log("Saved");
+        print(json);
+    }
+}
+
+[System.Serializable]
+public class SaveColor {
+    public int[] num = new int[11];
+    public string[] rgba = new string[11];
+}
