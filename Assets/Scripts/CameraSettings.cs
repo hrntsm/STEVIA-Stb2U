6ff74c22e2e0aee3c91a602864db1049e31d7a65
hiron_rgkr <contact@hrntsm.com>
2020-02-13 17:59:06 +0900

    [add] Setting from UI  the focal length of the camera
    カメラの焦点距離を設定できるようにした。

---
+++ Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraSettings : MonoBehaviour{
+    InputField m_inputField;
+
+    void Start() {
+        m_inputField = GetComponent<InputField>();
+    }
+    public void SetLensLength() {
+        Camera cam = Camera.main;
+        cam.focalLength = float.Parse(m_inputField.text);
+    }
+}
