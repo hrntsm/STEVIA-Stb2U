591a11e67bb107bd632bb746b62855d832d6c3f0
hiron <contact@hrntsm.com>
2020-02-23 22:32:34 +0900

    [add] UI for showing / hiding element
    部材の表示/非表示についてのUIを追加

---
+++ Assets/Scripts/DisplySettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DisplySettings : MonoBehaviour {
+    Toggle m_toggle;
+    GameObject m_dispObject;
+    string m_findName = "";
+    bool m_hasObj = false;
+
+    void Start() {
+        m_toggle = GetComponent<Toggle>();
+    }
+
+    public void ElementDisp(int index) {
+        switch (index) {
+            case 0: m_findName = "StbColumns"; break;
+            case 1: m_findName = "StbColumnBar"; break;
+            case 2: m_findName = "StbGirders"; break;
+            case 3: m_findName = "StbGirderBar"; break;
+            case 4: m_findName = "StbPosts"; break;
+            case 5: m_findName = "StbPostBar"; break;
+            case 6: m_findName = "StbBeams"; break;
+            case 7: m_findName = "StbBeamBar"; break;
+            case 8: m_findName = "StbBraces"; break;
+            case 9: m_findName = "StbBraceBar"; break;
+            case 10: m_findName = "StbSlabs"; break;
+            case 11: m_findName = "StbSlabBar"; break;
+            default: break;
+        }
+        if (m_hasObj == false) {
+            m_dispObject = GameObject.Find(m_findName);
+            m_hasObj = true;
+        }
+        m_dispObject.SetActive(m_toggle.isOn);
+    }
+}
