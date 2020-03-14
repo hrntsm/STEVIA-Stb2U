60ce85bd3a77c725b021b52d5d8059db0c2a9359
hiron <contact@hrntsm.com>
2020-03-14 15:50:32 +0900

    [add] VR model Calibrater
    簡易的なキャリブレーションを追加
    実装は頭の位置を合わせているだけ

---
+++ Assets/Scripts/Calibrator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stevia {
+    public class Calibrator:MonoBehaviour {
+
+        [SerializeField]
+        Transform cameraRig;
+
+        [SerializeField]
+        Transform hmdEye;
+
+        [SerializeField, Range(0f, 3.0f)]
+        float charaHeight;
+
+        public void Calibrate() {
+            float height = charaHeight - hmdEye.transform.localPosition.y;
+            cameraRig.position = new Vector3(0, height, 0);
+        }
+    }
+}
