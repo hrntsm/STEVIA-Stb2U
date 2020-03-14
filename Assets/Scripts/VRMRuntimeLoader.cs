f16c1ab1fefd1bd2e9d1aff6f1e6860db931c589
hiron <contact@hrntsm.com>
2020-03-14 14:41:08 +0900

    [add] blinker Setting
    VRMにblinkerを付けるように設定

--- Assets/Scripts/VRMRuntimeLoader.cs
+++ Assets/Scripts/VRMRuntimeLoader.cs
@@ -9,7 +9,7 @@ using RootMotion.FinalIK;
 namespace Stevia {
 
     public class VRMRuntimeLoader:MonoBehaviour {
-        GameObject m_model;
+        GameObject _model;
 
         void Start() {
             LoadVRM();
@@ -41,37 +41,40 @@ namespace Stevia {
         }
 
         void OnLoaded(VRMImporterContext context) {
-            if (m_model != null) {
-                GameObject.Destroy(m_model.gameObject);
+            if (_model != null) {
+                GameObject.Destroy(_model.gameObject);
             }
 
-            m_model = context.Root;
-            m_model.transform.position = new Vector3(-10, 0, 0);
-            m_model.transform.rotation = Quaternion.Euler(0, 270, 0);
+            _model = context.Root;
+            _model.transform.position = new Vector3(-10, 0, 0);
+            _model.transform.rotation = Quaternion.Euler(0, 270, 0);
 
             context.ShowMeshes();
             context.EnableUpdateWhenOffscreen();
 
             // VR化用に追記
-            m_model.AddComponent<VRIK>();
-            m_model.GetComponent<VRIK>().solver.spine.headTarget = GameObject.Find("Head_Target").transform;
-            m_model.GetComponent<VRIK>().solver.leftArm.target = GameObject.Find("Hand_L_Target").transform;
-            m_model.GetComponent<VRIK>().solver.rightArm.target = GameObject.Find("Hand_R_Target").transform;
+            _model.AddComponent<VRIK>();
+            _model.GetComponent<VRIK>().solver.spine.headTarget = GameObject.Find("Head_Target").transform;
+            _model.GetComponent<VRIK>().solver.leftArm.target = GameObject.Find("Hand_L_Target").transform;
+            _model.GetComponent<VRIK>().solver.rightArm.target = GameObject.Find("Hand_R_Target").transform;
 
             // akiraさんのQiitaよりがに股を直すように設定
-            m_model.GetComponent<VRIK>().solver.leftLeg.swivelOffset = 15f;
-            m_model.GetComponent<VRIK>().solver.rightLeg.swivelOffset = -15f;
-            m_model.GetComponent<VRIK>().solver.locomotion.footDistance = 0.4f;
-            m_model.GetComponent<VRIK>().solver.locomotion.stepThreshold = 0.3f;
-            m_model.GetComponent<VRIK>().solver.locomotion.maxVelocity = 0.3f;
-            m_model.GetComponent<VRIK>().solver.locomotion.rootSpeed = 30f;
-            m_model.GetComponent<VRIK>().solver.plantFeet = false;
+            _model.GetComponent<VRIK>().solver.leftLeg.swivelOffset = 15f;
+            _model.GetComponent<VRIK>().solver.rightLeg.swivelOffset = -15f;
+            _model.GetComponent<VRIK>().solver.locomotion.footDistance = 0.4f;
+            _model.GetComponent<VRIK>().solver.locomotion.stepThreshold = 0.3f;
+            _model.GetComponent<VRIK>().solver.locomotion.maxVelocity = 0.3f;
+            _model.GetComponent<VRIK>().solver.locomotion.rootSpeed = 30f;
+            _model.GetComponent<VRIK>().solver.plantFeet = false;
 
             //自分のカメラに頭が映らないように VRMFirstPerson の設定
-            m_model.GetComponent<VRM.VRMFirstPerson>().Setup();
+            _model.GetComponent<VRM.VRMFirstPerson>().Setup();
             foreach (var renderer in GetComponentsInChildren<SkinnedMeshRenderer>(true)) {
                 renderer.updateWhenOffscreen = true;
             }
+
+            // まばたきをする設定
+            _model.AddComponent<VRM.Blinker>();
         }
     }
 }
