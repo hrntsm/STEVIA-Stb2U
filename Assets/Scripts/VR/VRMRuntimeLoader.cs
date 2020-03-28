35f49ea5b11fd8146f7c65cfad200d942de436ee
hiron <contact@hrntsm.com>
2020-03-28 17:35:11 +0900

    [updata] the movement is strange when you put your feet on
    足をつかせると動きが変なので修正

--- Assets/Scripts/VR/VRMRuntimeLoader.cs
+++ Assets/Scripts/VR/VRMRuntimeLoader.cs
@@ -71,9 +71,6 @@ namespace Stevia.VR {
             _model.GetComponent<VRIK>().solver.locomotion.rootSpeed = 30f;
             _model.GetComponent<VRIK>().solver.plantFeet = false;
 
-            // キャリブレーションなくしたので足は地面につける
-            _model.GetComponent<VRIK>().solver.plantFeet = true;
-
             //自分のカメラに頭が映らないように VRMFirstPerson の設定
             _model.GetComponent<VRM.VRMFirstPerson>().Setup();
             foreach (var renderer in GetComponentsInChildren<SkinnedMeshRenderer>(true)) {
